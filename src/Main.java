386e0fbb41cb1344442c79ed097a33adf009c513
mxx567 <themaks567@gmail.com>
2026-09-19 16:01:05 +0500

    Recurse on the smaller partition and iterate over the larger one method for QuickSort

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import algorithms.*;
 void main(String[] args) {
     int[] arr = {1,3,2,10,7};
-    MergeSort.mergeSort(arr);
+    QuickSort.quickSort(arr);
     for(int i = 0; i < arr.length; i++){
         System.out.print(arr[i] +" ");
     }
--- src/algorithms/QuickSort.java
+++ src/algorithms/QuickSort.java
@@ -8,16 +8,29 @@ public class QuickSort {
         quickSort(arr, 0, arr.length - 1);
     }
     public static void quickSort(int[] arr, int start, int end){
-        if(end <= start){
-            return;
+        while(start < end){
+            int pivin = start + random.nextInt(end - start + 1);
+            int temp = arr[pivin];
+            arr[pivin] = arr[end];
+            arr[end] = temp;
+            int pivot = partition(arr, start, end);
+            if (pivot - start < end - pivot) {
+                quickSort(arr, start, pivot - 1);
+                start = pivot + 1;
+
+            } else {
+                quickSort(arr, pivot + 1, end);
+                end = pivot - 1;
+            }
+
+            quickSort(arr, start, pivot - 1);
+            quickSort(arr, pivot + 1, end);
         }
-        int pivot = partition(arr, start, end);
-        quickSort(arr, start, pivot - 1);
-        quickSort(arr, pivot + 1, end);
+
     }
 
     public static int partition(int[] arr, int start, int end){
-        int pivot = start + random.nextInt(end - start + 1);
+        int pivot = arr[end];
         int i = start -1;
         for(int j = start; j <= end - 1; j++){
             if(arr[j] < pivot){
