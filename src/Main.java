ab4e4564a8c8ab990047f1a8ed9ba97eae93491c
mxx567 <themaks567@gmail.com>
2026-09-19 20:47:50 +0500

    ClosestPairSolver has been implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 import algorithms.*;
+import objects.*;
 void main(String[] args) {
     int[] arr = {1,3,2,10,7,82,12,34,21,42,30,55,21};
-    System.out.println(DeterministicSelector.detSel(arr, 0, arr.length-1, 5));
+    Point[] points = {
+            new Point(0, 0),
+            new Point(2, 10),
+            new Point(4, 0),
+            new Point(6, 10),
+            new Point(8, 0)
+    };
+    ClosestPairSolver.insertionSort(points, 0, points.length-1, true);
+
+    System.out.println(ClosestPairSolver.closestPair(points, 0, points.length-1));
 }
---
+++ src/algorithms/ClosestPairSolver.java
@@ -0,0 +1,75 @@
+package algorithms;
+import objects.Point;
+
+public class ClosestPairSolver {
+    public static double closestPair(Point[] points, int l, int r){
+        if (r - l + 1 <= 2) {
+            if (r - l + 1 == 2) {
+                return getDistance(points[l], points[r]);
+            }
+            return Double.MAX_VALUE;
+        }
+        else{
+            int mid = l + (r-l)/2;
+            double dA = closestPair(points, l,mid);
+            double dB = closestPair(points, mid+1, r);
+            double d = Math.min(dA,dB);
+            double midX = points[mid].getX();
+            Point[] strip = new Point[r - l + 1];
+            int size = 0;
+
+            for (int i = l; i <= r; i++) {
+                if (Math.abs(points[i].getX() - midX) < d) {
+                    strip[size++] = points[i];
+                }
+            }
+            insertionSort(strip, 0, size - 1, false);
+
+            for (int i = 0; i < size; i++) {
+                for (int j = i + 1; j < size; j++) {
+                    if (strip[j].getY() - strip[i].getY() >= d) {
+                        break;
+                    }
+
+                    d = Math.min(d, getDistance(strip[i], strip[j]));
+                }
+            }
+
+            return d;
+        }
+    }
+
+    public static void insertionSort(Point[] arr, int l, int r, boolean byX) {
+        if(byX){
+            for (int i = l + 1; i <= r; i++) {
+                Point key = arr[i];
+                int j = i - 1;
+
+                while (j >= l && arr[j].getX() > key.getX()) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+        else {
+            for (int i = l + 1; i <= r; i++) {
+                Point key = arr[i];
+                int j = i - 1;
+
+                while (j >= l && arr[j].getY() > key.getY()) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+
+                arr[j + 1] = key;
+            }
+        }
+    }
+
+    private static double getDistance(Point p1, Point p2){
+        return Math.sqrt((p2.getX()- p1.getX()) * (p2.getX()- p1.getX()) +
+                (p2.getY()- p1.getY()) * (p2.getY()- p1.getY()));
+    }
+}
